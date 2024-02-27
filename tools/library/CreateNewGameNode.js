1a53f5c4ae188cb22cbe7c5ef373ba268085d635
sso <93847254+ssoq@users.noreply.github.com>
2024-02-27 07:44:41 +0000

    testing stuff

--- tools/library/CreateNewGameNode.js
+++ tools/library/CreateNewGameNode.js
@@ -53,3 +53,8 @@ async function CreateNewGameNode(id, coverUrl, intFormat, navigate, navType, pag
 }
 
 CreateNewGameNode(3260590327, "https://tr.rbxcdn.com/a96d6e2ef1e82fea54546646a10c51ac/768/432/Image/Png", TypeOfIntegerFormat.Delimiters, CanNavigateUser.True, TypeOfTarget.Blank, TypeOfPage.Game);
+CreateNewGameNode(9571630408, "https://tr.rbxcdn.com/ad02ebb0502100d13d2702d87f671243/768/432/Image/Png", TypeOfIntegerFormat.Delimiters, CanNavigateUser.True, TypeOfTarget.Blank, TypeOfPage.Game);
+CreateNewGameNode(183364845, "https://tr.rbxcdn.com/c54936c4c6a8cf4279d63c0abf7ee44b/768/432/Image/Png", TypeOfIntegerFormat.Delimiters, CanNavigateUser.True, TypeOfTarget.Blank, TypeOfPage.Game);
+CreateNewGameNode(8737899170, "https://tr.rbxcdn.com/b4f3eb329b52b3eb90185b651867471b/768/432/Image/Png", TypeOfIntegerFormat.Delimiters, CanNavigateUser.True, TypeOfTarget.Blank, TypeOfPage.Game);
+CreateNewGameNode(189707, "https://tr.rbxcdn.com/a540a426f3c58ee6d773a0b47304b445/768/432/Image/Png", TypeOfIntegerFormat.Delimiters, CanNavigateUser.True, TypeOfTarget.Blank, TypeOfPage.Game);
+CreateNewGameNode(6447798030, "https://tr.rbxcdn.com/1dff6f1dcf27be6899afa2b6c1b7a789/768/432/Image/Png", TypeOfIntegerFormat.Delimiters, CanNavigateUser.True, TypeOfTarget.Blank, TypeOfPage.Game);
